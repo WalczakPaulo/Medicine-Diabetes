852bb24b4f6ae98e62ac69bec434911f931c4fcb
Paul <walczakp007@gmail.com>
2017-05-01 20:15:41 +0200

    Spliting datasets

--- src/read_dataset.py
+++ src/read_dataset.py
@@ -1,5 +1,6 @@
 import csv
-
+from random import shuffle
+import numpy as np
 '''
 Data Format:
 1. Number of times pregnant.
@@ -11,16 +12,32 @@ Data Format:
 7. Diabetes pedigree function
 8. Age (years)
 9. Class variable (0 or 1) 
-'''
 
-'''
 read_data() returns list of lists:
 [['6', '148', '72', '35', '0', '33.6', '0.627', '50', '1'],...],
+
+read_and_split_data() returns training and testing sets
 '''
+
+
 def read_data():
     dataset = []
     with open('data.csv') as csvfile:
         rows = csv.reader(csvfile)
         dataset = list(rows)
     return dataset
+
+
+def read_and_split_data():
+    dataset = read_data()
+
+    shuffle(dataset)
+    dataset = np.array(dataset)
+    training_size = int(0.9*len(dataset))
+    test_x = list(dataset[:-training_size][:, :-1])
+    test_y = list(dataset[:-training_size][:, -1])
+    train_x = list(dataset[-training_size:][:, :-1])
+    train_y = list(dataset[-training_size:][:,-1])
+
+    return train_x, train_y, test_x, test_y
 
